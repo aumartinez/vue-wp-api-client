75c9fbc14db4c85c0e70063115886eacb0abc0bc
aumartines <acm_asd@hotmail.com>
2022-05-02 12:31:21 -0600

    Adding footer

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Header /> 
+    <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {  
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/app/components/Footer.vue
@@ -0,0 +1,143 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img alt="footer logo" class="img-responsive" src="img/logo.png" />
+          
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          </p>
+          
+          <ul class="list-inline social-icons">
+            <li class="list-inline-item">
+              <a href="#" title="Facebook" class="icon bg-secondary color-white">
+                <i class="fa-brands fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" title="LinkedIn" class="icon bg-secondary color-white">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" title="Instagram" class="icon bg-secondary color-white">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" title="Skype" class="icon bg-secondary color-white">
+                <i class="fa-brands fa-skype"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <div class="row">
+            <div class="col-md-6">
+              <h4>
+                Pages
+              </h4>
+              
+              <nav>
+                <ul class="nav flex-column"
+                v-for="item in menu"
+                :key="item.ID"
+                >
+                  <li class="nav-item">
+                    <a class="nav-link" :href="item.url">
+                    {{item.title}}
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            
+            <div class="col-md-6">
+              <h4>
+                Recent Episodes
+              </h4>
+              
+              <nav>
+                <ul class="nav flex-column">
+                  <li class="nav-item"
+                  v-for="index in 4"
+                  :key="index"
+                  >
+                    <a class="nav-link" 
+                    :href="recent[index].link">
+                     {{recent[index].title.rendered}}
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div><!--/row-->
+        </div>
+        <div class="col-md-4">
+          <h4>
+            Listen my podcasts also in
+          </h4>
+          
+          <ul class="footer-cards-wrapper flex-column list-group">
+            <li class="podcast-card list-group-item">
+              <a href="#">
+                <img class="img-responsive" alt="Google icon" src="img/listen-google.png" />
+              </a>
+            </li>              
+            <li class="podcast-card list-group-item">
+              <a href="#">
+                <img class="img-responsive" alt="Apple icon" src="img/listen-apple.png" />
+              </a>
+            </li>
+            <li class="podcast-card list-group-item">
+              <a href="#">
+                <img class="img-responsive" alt="Spotify icon" src="img/listen-spotify.png" />
+              </a>
+            </li>              
+          </ul>
+        </div>
+        <div class="col-12">
+          <div class="footer-line">
+          </div>
+          <p class="copyright text-center">
+            &copy; 2022 NicaSource All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer> 
+</template>
+
+<script>
+import {links} from '../footer.js';
+
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      menu: [],
+      recent: []
+    }
+  },
+  mounted () {
+    let res = links.getLinks();
+    let getMenu = () => {
+      res.then(result => {
+        this.menu = result;
+      });
+    };
+    
+    getMenu();
+    
+    res = links.getLatest();    
+    let getEpisodes = () => {
+      res.then(result => {
+        this.recent = result;
+      });
+    };
+    
+    getEpisodes();
+  }
+}
+</script>
