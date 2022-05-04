3c70f2ad7795dbded5648397fe81a0357a9f8122
Augusto Martinez <acm_asd@hotmail.com>
2022-05-03 19:21:37 -0600

    Add files via upload

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -1,9 +1,19 @@
-//menu.js
+//footer.js
 import axios from 'axios';
 
 export const links = {
    async getLinks() {
       let url = 'http://localhost:8090/php-headless-wp/wordpress/wp-json/myroutes/menu';
+      
+      let resp =  await axios
+                        .get(url)
+                        .then(res => res.data)
+                        .catch(error => error);
+      
+      return resp;
+    },
+    async getLatest() {
+      let url = 'http://localhost:8090/php-headless-wp/wordpress/wp-json/wp/v2/posts?orderby=date';
       
       let resp =  await axios
                         .get(url)
--- docs/js/gallery.js
+++ docs/js/gallery.js
@@ -1,9 +1,9 @@
-//menu.js
+//gallery.js
 import axios from 'axios';
 
-export const links = {
-   async getLinks() {
-      let url = 'http://localhost:8090/php-headless-wp/wordpress/wp-json/myroutes/menu';
+export const gallery = {
+   async getContent() {
+      let url = 'http://localhost:8090/php-headless-wp/wordpress/wp-json/wp/v2/pages/?slug=gallery';
       
       let resp =  await axios
                         .get(url)
